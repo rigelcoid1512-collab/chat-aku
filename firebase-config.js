bd2f423084f006c7dad5d8d1339e13b94ad0607b
rigelcoid1512-collab <rigel.co.id.1512@gmail.com>
2025-08-20 06:24:00 +0700

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Konfigurasi Firebase
+const firebaseConfig = {
+  apiKey: "ISI-DENGAN-API-KEY",
+  authDomain: "chat-aku.firebaseapp.com",
+  databaseURL: "https://chat-aku-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-aku",
+  storageBucket: "chat-aku.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef123456"
+};
+
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
